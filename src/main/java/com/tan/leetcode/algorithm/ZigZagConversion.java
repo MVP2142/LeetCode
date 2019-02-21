741e8cffff2156553bd0633898c4edc82e5e86bd
zhuojun.tian <wx01@ubssdic.com>
2019-02-21 19:50:44 +0800

    添加练习

---
+++ src/main/java/com/tan/leetcode/algorithm/ZigZagConversion.java
@@ -0,0 +1,17 @@
+package com.tan.leetcode.algorithm;
+
+/**
+ * @author zhuojun.tian
+ * @since 2019/2/21
+ */
+public class ZigZagConversion {
+    public static String convert(String s, int numRows) {
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        convert("PAHNAPLSIIGYIR", 3);
+    }
+}
